33db50f7815c115d0a7ab78cab17d6edbe63185c
Panco <panco@pancic.com>
2022-02-12 23:15:17 +0100

    Dodavanje slika za primer 1.

---
+++ primeri/primer1/src/ts/components/App.tsx
@@ -0,0 +1,15 @@
+export default function App() {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    );
+}
+
+/*
+    <div>
+        <i className='fa-solid fa-star'></i>
+        <i className='fa-solid fa-star'></i>
+        <i className='fa-solid fa-star-half-stroke'></i>
+    </div>
+*/
